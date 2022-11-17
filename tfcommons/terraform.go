e20377b2592312f64c517d710cd5235f726fd6ab
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2022-11-17 11:29:42 -0600

    Use regex for excludepaths when finding terraform modules
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

--- tfcommons/terraform.go
+++ tfcommons/terraform.go
@@ -3,20 +3,30 @@ package tfcommons
 import (
 	"os"
 	"path/filepath"
+	"regexp"
 	"sort"
-	"strings"
 )
 
 // FindTerraformModules walks the local folder looking for Terraform modules. A Terraform module is any folder
 // that has a .tf file.
 //
-// excludePaths is a slice of paths relative to the root that should be excluded from the search. For example, if you
-// wish to ignore the folder `examples` in a repository root, add the string `examples` in the `excludPaths` slice.
+// excludePaths is a slice of path regexes relative to the root that should be excluded from the search. For example, if
+// you wish to ignore the folder `examples` in a repository root, add the string `^examples` in the `excludPaths` slice.
+// Or if you wish to ignore any .terraform paths: ".terraform".
 //
 // This will return a string slice of relative paths from the root that contains a Terraform module.
 func FindTerraformModules(root string, excludePaths []string) ([]string, error) {
 	// Use a map for tracking module paths so that it can be deduped
 	modulePathTracker := make(map[string]bool)
+
+	excludePathRegexes := make([]*regexp.Regexp, len(excludePaths))
+	for i, path := range excludePaths {
+		re, err := regexp.Compile(path)
+		if err != nil {
+			return nil, err
+		}
+		excludePathRegexes[i] = re
+	}
 
 	// Walk the root path, looking for any folders that have a .tf file.
 	err := filepath.Walk(
@@ -31,8 +41,8 @@ func FindTerraformModules(root string, excludePaths []string) ([]string, error) 
 			if err != nil {
 				return err
 			}
-			for _, ignorePath := range excludePaths {
-				if strings.HasPrefix(relPath, ignorePath) {
+			for _, re := range excludePathRegexes {
+				if re.MatchString(relPath) {
 					return nil
 				}
 			}
