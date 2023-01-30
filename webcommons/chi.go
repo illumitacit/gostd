2878f53b3f03dff06a094ce0b996e419bdc5bba3
Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>
2023-01-30 12:08:00 -0600

    Add utility function for initializing go-chi (#32)
    Signed-off-by: Yoriyasu Yano <430092+yorinasub17@users.noreply.github.com>

---
+++ webcommons/chi.go
@@ -0,0 +1,32 @@
+package webcommons
+
+import (
+	"github.com/fensak-io/httpzaplog"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"go.uber.org/zap"
+)
+
+// NewChiRouterWithDefaultMiddlewares returns a new go-chi router that has a set of recommended default routers
+// configured.
+func NewChiRouterWithDefaultMiddlewares(logger *zap.Logger) chi.Router {
+	router := chi.NewRouter()
+	AddDefaultMiddlewares(router, logger)
+	return router
+}
+
+// AddDefaultMiddlewares adds all the chi middlewares that we want for applied to all requests.
+func AddDefaultMiddlewares(router chi.Router, logger *zap.Logger) {
+	logOpts := &httpzaplog.Options{
+		Logger: logger,
+
+		// TODO: make configurable
+		Concise:     false,
+		SkipHeaders: nil,
+	}
+
+	router.Use(middleware.RealIP)
+
+	// NOTE: httpzaplog includes RequestID and Recoverer middlewares
+	router.Use(httpzaplog.RequestLogger(logOpts))
+}
